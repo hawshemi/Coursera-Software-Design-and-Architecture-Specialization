fce80cdb4ad6fb7302c1cbf390d9416439c063af
R Hashemi <rhawshemi@gmail.com>
2023-01-02 17:08:20 +0330

    Added Assignments.
    Added Assignments.

--- 2. Design Patterns/Week 2/Capstone Assignment 2.1/EditContactCommand.java
+++ 2. Design Patterns/Week 2/Capstone Assignment 2.1/EditContactCommand.java
@@ -4,23 +4,24 @@ import android.content.Context;
 
 // By https://hawshemi.com on 01-Jan-2023
 
+// Command to edit a pre-existing contact
 public class EditContactCommand extends Command {
-    private ContactList contactList;
-    private Contact oldContact;
-    private Contact newContact;
+
+    private ContactList contact_list;
+    private Contact old_contact;
+    private Contact new_contact;
     private Context context;
 
-    public EditContactCommand(ContactList contactList, Contact oldContact, Contact newContact, Context context) {
-        this.contactList = contactList;
-        this.oldContact = oldContact;
-        this.newContact = newContact;
+    public EditContactCommand(ContactList contact_list, Contact old_contact, Contact new_contact, Context context) {
+        this.contact_list = contact_list;
+        this.old_contact = old_contact;
+        this.new_contact = new_contact;
         this.context = context;
     }
 
-    @Override
     public void execute() {
-        contactList.deleteContact(oldContact);
-        contactList.addContact(newContact);
-        setIsExecuted(contactList.saveContacts(context));
+        contact_list.deleteContact(old_contact);
+        contact_list.addContact(new_contact);
+        setIsExecuted(contact_list.saveContacts(context));
     }
 }
