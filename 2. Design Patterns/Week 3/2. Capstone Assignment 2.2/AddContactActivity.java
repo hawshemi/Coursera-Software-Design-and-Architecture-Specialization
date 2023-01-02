fd77cb911a1e56c5491333d32671d245245284d3
R Hashemi <rhawshemi@gmail.com>
2023-01-02 07:50:07 -0600

    Added Assignments.
    Added Assignments.

---
+++ 2. Design Patterns/Week 3/2. Capstone Assignment 2.2/AddContactActivity.java
@@ -0,0 +1,65 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+// By https://hawshemi.com on 01-Jan-2023
+
+// Add a new contact
+public class AddContactActivity extends AppCompatActivity {
+
+    private ContactList contact_list = new ContactList();
+    private ContactListController contact_list_controller = new ContactListController(contact_list);
+
+    private Context context;
+
+    private EditText username;
+    private EditText email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+
+        username = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+
+        context = getApplicationContext();
+        contact_list_controller.loadContacts(context);
+    }
+
+    public void saveContact(View view) {
+
+        String username_str = username.getText().toString();
+        String email_str = email.getText().toString();
+
+        if (username_str.equals("")) {
+            username.setError("Empty field!");
+            return;
+        }
+
+        if (!email_str.contains("@")) {
+            email.setError("Must be an email address!");
+            return;
+        }
+
+        if (!contact_list_controller.isUsernameAvailable(username_str)){
+            username.setError("Username already taken!");
+            return;
+        }
+
+        Contact contact = new Contact(username_str, email_str, null);
+
+        // Add contact
+        boolean success = contact_list_controller.addContact(contact, context);
+        if (!success) {
+            return;
+        }
+
+        // End AddContactActivity
+        finish();
+    }
+}
